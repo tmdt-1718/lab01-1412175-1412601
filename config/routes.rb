5e1461bb7012c995edc041830b374de8869bcb6e
Bill <baotuan0.0@gmail.com>
2017-10-09 10:05:58 +0700

    Added album page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'about/index', as: :about
+  get 'about',to: 'about#index', as: :about
+  get 'album',to: 'album#index', as: :album
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
