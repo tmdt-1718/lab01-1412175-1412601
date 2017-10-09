d97a5a29a91f9fcd027e7ae10d0f4e9c6aa8d772
Bill <baotuan0.0@gmail.com>
2017-10-09 10:56:16 +0700

    Added show detail album

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get 'about',to: 'about#index', as: :about
-  get 'album',to: 'album#index', as: :album
+  # get 'album',to: 'album#index', as: :album
+  resources :album, only: [:index, :show]
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
