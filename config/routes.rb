ae1078996d5449ebd6405373a24e05af434e8d35
Bill <baotuan0.0@gmail.com>
2017-10-09 22:03:52 +0700

    Added post blog

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'about',to: 'about#index', as: :about
   resources :album, only: [:index, :show]
-  resources :blog, only: [:index, :show]
+  resources :blog, only: [:index, :show, :create, :new]
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.delete_all
 Article.delete_all
 
 (1..10).each do |user_num|
-  user = User.create!(name: Faker::Name.name, email: "baotuan0.0@gmail.com", password_digest: "123")
+  user = User.create!(name: Faker::Name.name, email: "baotuan0.#{user_num}@gmail.com", password_digest: "123")
 
   (1..10).each do |article_num|
     article = user.articles.create!(title: Faker::Pokemon.name, body: Faker::Lorem.paragraph(10))
