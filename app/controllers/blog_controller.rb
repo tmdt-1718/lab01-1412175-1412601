e04dcc65682f49ad66052ea6964dc6cb63d286a2
Bill <baotuan0.0@gmail.com>
2017-10-09 22:31:06 +0700

    Finish add new blog

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,15 +1,17 @@
 class BlogController < ApplicationController
 
   def new
-    
+    if !current_user
+      redirect_to login_path
+    end
   end
 
   def create
     begin
-      blog = Article.create!(blog_params)
+      @blog = Article.create!(title: params[:article][:title], body: params[:article][:body], user_id: current_user.id)
       flash[:success] = "Create new blog successfully"
 
-      render blog_path(blog)
+      redirect_to blog_path(@blog)
     rescue ActiveRecord::RecordInvalid => e
       flash[:error] = "Create new blog fail."
       redirect :index
